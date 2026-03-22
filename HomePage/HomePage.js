11b3a446945c703bb6f01db12d237b8b91a67a10
Quatov <nooasarano21@gmail.com>
2026-03-22 16:02:20 +0500

    Added features for CTR

--- HomePage/HomePage.js
+++ HomePage/HomePage.js
@@ -1,3 +1,10 @@
+window.addEventListener("load", () => {
+    fetch("http://localhost:5000/api/analytics/impression", {
+        method: "POST"
+    })
+        .catch(err => console.error("Impression error:", err));
+});
+
 const button = document.getElementById("chatBtn");
 
 button.addEventListener("click", function(e) {
@@ -18,3 +25,21 @@ button.addEventListener("click", function(e) {
 
   button.appendChild(circle);
 });
+
+const chatButton = document.getElementById("chatBtn");
+
+if (chatButton) {
+    chatButton.addEventListener("click", async (e) => {
+        e.preventDefault(); // block the transition
+
+        try {
+            await fetch("http://localhost:5000/api/analytics/click", {
+                method: "POST"
+            });
+
+            alert("🚧 Сервис в разработке. Скоро будет доступен!")
+        } catch (error) {
+            console.error("Click error:", error);
+        }
+    })
+}
