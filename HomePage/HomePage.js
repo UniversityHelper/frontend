c6bf9f469398e8aa8927b88304efec0f9bcf08a2
Nadezhda9 <nvsnadya11@gmail.com>
2026-03-22 12:21:22 +0500

    начальная страничка

---
+++ HomePage/HomePage.js
@@ -0,0 +1,20 @@
+const button = document.getElementById("chatBtn");
+
+button.addEventListener("click", function(e) {
+  let circle = document.createElement("span");
+  let diameter = Math.max(button.clientWidth, button.clientHeight);
+  let radius = diameter / 2;
+
+  circle.style.width = circle.style.height = `${diameter}px`;
+  circle.style.left = `${e.clientX - button.offsetLeft - radius}px`;
+  circle.style.top = `${e.clientY - button.offsetTop - radius}px`;
+  
+  circle.classList.add("ripple");
+
+  const ripple = button.getElementsByClassName("ripple")[0];
+  if (ripple) {
+    ripple.remove();
+  }
+
+  button.appendChild(circle);
+});
