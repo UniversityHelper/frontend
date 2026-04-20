6e7ff6ca424c9b01ea0b391290a5dc3ce6989bb1
Nadezhda9 <nvsnadya11@gmail.com>
2026-04-20 14:50:55 +0500

    adaptability

--- HomePage/HomePage.js
+++ HomePage/HomePage.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", () => {
+/*window.addEventListener("load", () => {
     fetch("http://localhost:5000/api/analytics/impression", {
         method: "POST"
     })
@@ -42,4 +42,48 @@ if (chatButton) {
 
         window.location.href = "../ChatPage/ChatPage.html";
     })
-}
+}*/
+
+// отправка просмотра страницы
+window.addEventListener("load", () => {
+    fetch("http://localhost:5000/api/analytics/impression", {
+        method: "POST"
+    }).catch(err => console.error("Impression error:", err));
+});
+
+// все кнопки
+const buttons = document.querySelectorAll(".chat-button");
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", async (e) => {
+        e.preventDefault();
+
+        // ripple эффект
+        let circle = document.createElement("span");
+        let diameter = Math.max(button.clientWidth, button.clientHeight);
+        let radius = diameter / 2;
+
+        circle.style.width = circle.style.height = `${diameter}px`;
+        circle.style.left = `${e.clientX - button.offsetLeft - radius}px`;
+        circle.style.top = `${e.clientY - button.offsetTop - radius}px`;
+
+        const ripple = button.querySelector("span");
+        if (ripple) ripple.remove();
+
+        button.appendChild(circle);
+
+        // аналитика
+        try {
+            await fetch("http://localhost:5000/api/analytics/click", {
+                method: "POST"
+            });
+        } catch (error) {
+            console.error("Click error:", error);
+        }
+
+        // переход
+        window.location.href = button.href;
+    });
+
+});
