cf31941b9e433657f174a1f644dca07cd61213c6
Quatov <nooasarano21@gmail.com>
2026-05-03 23:44:23 +0500

    Revert "added protection"
    This reverts commit 525e4f42ec8ed68df5d784860c463f6f3947e64a.

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.API_CONFIG = {
-    API_URL: "https://unihelper-backend-2xlp1d-c53fb4-81-26-177-175.traefik.me"
+    API_URL: "http://unihelper-backend-2xlp1d-c53fb4-81-26-177-175.traefik.me"
 };
